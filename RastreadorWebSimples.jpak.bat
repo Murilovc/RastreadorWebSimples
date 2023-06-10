f4a6b49c3ae151beb6910645b37c00f38d5d2744
Murilo Vieira <mvc.dev7cf@gmail.com>
2023-06-10 11:17:54 -0500

    Bat para criação de instalador usando jpackage

---
+++ RastreadorWebSimples.jpak.bat
@@ -0,0 +1,3 @@
+echo Executando o comando: 
+jpackage --input . --name RastreadorWebSimples --app-version 0.5 --win-dir-chooser --win-shortcut --main-jar RastreadorWebSimples.jar
+echo Sucesso na criacao do pacote de instalacao 
